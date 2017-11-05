d30685ed7c4c958f09db16e11f1ec2effc3db41e
Mikkel Møller Brusen <Mikkel_mb@hotmail.com>
2017-11-05 16:45:53 +0100

    Check if graph is tree

---
+++ src/com/company/CheckUndirectedGraphisTree.java
@@ -0,0 +1,146 @@
+package com.company;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+class TGraph
+{
+    static final int MAXV      = 100;
+    static final int MAXDEGREE = 50;
+    public int       edges[][] = new int[MAXV + 1][MAXDEGREE];
+    public int       degree[]  = new int[MAXV + 1];
+    public int       nvertices;
+    public int       nedges;
+
+    TGraph()
+    {
+        nvertices = nedges = 0;
+        for (int i = 1; i <= MAXV; i++)
+            degree[i] = 0;
+    }
+
+    void read_CCGraph()
+    {
+        int x, y;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of vertices: ");
+        nvertices = sc.nextInt();
+        System.out.println("Enter the number of edges: ");
+        int m = sc.nextInt();
+        System.out.println("Enter the edges: <from> <to>");
+        for (int i = 1; i <= m; i++)
+        {
+            x = sc.nextInt();
+            y = sc.nextInt();
+            insert_edge(x, y);
+        }
+        sc.close();
+    }
+
+    void insert_edge(int x, int y)
+    {
+        if (degree[x] > MAXDEGREE)
+            System.out.printf(
+                    "Warning: insertion (%d, %d) exceeds max degree\n", x, y);
+        edges[x][degree[x]] = y;
+        degree[x]++;
+        nedges++;
+    }
+
+    void print_CCGraph()
+    {
+        for (int i = 1; i <= nvertices; i++)
+        {
+            System.out.printf("%d: ", i);
+            for (int j = degree[i] - 1; j >= 0; j--)
+                System.out.printf(" %d", edges[i][j]);
+            System.out.printf("\n");
+        }
+    }
+}
+
+public class CheckUndirectedGraphisTree
+{
+    static final int MAXV         = 100;
+    static boolean   processed[]  = new boolean[MAXV];
+    static boolean   discovered[] = new boolean[MAXV];
+    static int       parent[]     = new int[MAXV];
+
+    static void bfs(TGraph g, int start)
+    {
+        Queue<Integer> q = new LinkedList<>();
+        int i, v;
+        q.offer(start);
+        discovered[start] = true;
+        while (!q.isEmpty())
+        {
+            v = q.remove();
+            // process_vertex(v);
+            processed[v] = true;
+            for (i = g.degree[v] - 1; i >= 0; i--)
+            {
+                if (!discovered[g.edges[v][i]])
+                {
+                    q.offer(g.edges[v][i]);
+                    discovered[g.edges[v][i]] = true;
+                    parent[g.edges[v][i]] = v;
+                }
+            }
+        }
+    }
+
+    static void initialize_search(TGraph g)
+    {
+        for (int i = 1; i <= g.nvertices; i++)
+        {
+            processed[i] = discovered[i] = false;
+            parent[i] = -1;
+        }
+    }
+
+    static void process_vertex(int v)
+    {
+        System.out.printf(" %d", v);
+    }
+
+    static int connected_components(TGraph g)
+    {
+        int c;
+        initialize_search(g);
+        c = 0;
+        for (int i = 1; i <= g.nvertices; i++)
+        {
+            if (!discovered[i])
+            {
+                c++;
+                // System.out.printf("Component %d:", c);
+                bfs(g, i);
+                // System.out.printf("\n");
+            }
+        }
+        return c;
+    }
+
+    static public void main(String[] args)
+    {
+        TGraph g = new TGraph();
+        g.read_CCGraph();
+        g.print_CCGraph();
+        boolean flag = false;
+        if (g.nedges == g.nvertices - 1)
+        {
+            flag = true;
+            if (connected_components(g) == 1 && flag == true)
+            {
+                System.out
+                        .println("Graph is a Tree, as graph is connected and Euler's criterion is satisfied.");
+            }
+        }
+        else
+        {
+            System.out
+                    .println("Graph is not a Tree, as Euler's criterion is not satisfied");
+        }
+    }
+}
