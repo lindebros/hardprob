f68457ebda272ae549b8acd59ae38575192f63b4
lindebros <math.linde1994@gmail.com>
2017-11-05 15:46:03 +0100

    Finding subsets of list of specific size.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static com.company.Subsets.getSubsets;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        List<Integer> superSet = new ArrayList<>();
+        for (int i = 1 ; i <= 30; i++){
+            superSet.add(i);
+        }
+        System.out.println(getSubsets(superSet,5));
     }
+
+
 }
